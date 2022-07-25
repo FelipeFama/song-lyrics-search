fd121f700090bc6a442c52f56e7c6d0d20948662
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-07-25 14:16:53 -0300

    refatoração do código

--- js/app.js
+++ js/app.js
@@ -5,34 +5,39 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container');
 
 const apiUrl = `https://api.lyrics.ovh`
 
-const getMoreSongs = async url => {
-  const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
-  const data = await response.json()
+const fetchData = async url => {
+  const response = await fetch(url)
+  return await response.json()
+}
 
+const getMoreSongs = async url => {
+  const data = await fetchData(`https://cors-anywhere.herokuapp.com/${url}`)
   insertSongsIntoPage(data)
 }
 
-const insertSongsIntoPage = songsInfo => {
-  songsContainer.innerHTML = songsInfo.data.map(song => `
+const insertNextAndPrevButtons = ({ prev, next }) => {
+  prevAndNextContainer.innerHTML = `
+    ${prev ? `<button class="btn" onclick="getMoreSongs('${prev}')">Anteriores</button>` : ''}
+    ${next ? `<button class="btn" onclick="getMoreSongs('${next}')">Próximas</button>` : ''}
+  `
+}
+const insertSongsIntoPage = ({ data, prev, next }) => {
+  songsContainer.innerHTML = data.map(({ artist: { name }, title }) => `
    <li class="song">
      <span class="song-artist">
-       <strong>${song.artist.name}</strong> - ${song.title}
+       <strong>${name}</strong> - ${title}
      </span>
      <button class="btn" 
-      data-artist="${song.artist.name}" 
-      data-song-title="${song.title}"
+      data-artist="${name}" 
+      data-song-title="${title}"
       >
        Ver letra
      </button>
    </li>
   `).join('')
 
-  if (songsInfo.prev || songsInfo.next) {
-    prevAndNextContainer.innerHTML = `
-     ${songsInfo.prev ? `<button class="btn" onclick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
-     
-     ${songsInfo.next ? `<button class="btn" onclick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
-     `
+  if (prev || next) {
+    insertNextAndPrevButtons({ prev, next })
     return
   }
 
@@ -40,34 +45,28 @@ const insertSongsIntoPage = songsInfo => {
 }
 
 const fetchSongs = async term => {
-  const response = await fetch(`${apiUrl}/suggest/${term}`)
-  const data = await response.json()
-
+  const data = await fetchData(`${apiUrl}/suggest/${term}`)
   insertSongsIntoPage(data)
 }
 
-form.addEventListener('submit', event => {
+const handleFormSubmit = event => {
   event.preventDefault()
 
   const searchTerm = searchInput.value.trim()
+  searchInput.value = ''
+  searchInput.focus()
 
   if (!searchTerm) {
-    songsContainer.innerHTML = `
-    < li class="warning-message" >
-      Por favor digite um termo válido.
-     </li >
-   `
+    songsContainer.innerHTML = `<li class="warning-message" >Por favor digite um termo válido.</li>`
     return
   }
 
   fetchSongs(searchTerm)
-})
+}
 
-const fetchLyrics = async (artist, songTitle) => {
-  const response = await fetch(`${apiUrl}/v1/${artist}/${songTitle}`)
-  const data = await response.json()
-  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '</br>')
+form.addEventListener('submit', handleFormSubmit)
 
+const insertLyricsIntoPage = ({ artist, lyrics, songTitle }) => {
   songsContainer.innerHTML = `
    <li class="lyrics-container">
      <h2><strong>${songTitle}</strong> - ${artist}</h2>
@@ -76,7 +75,13 @@ const fetchLyrics = async (artist, songTitle) => {
   `
 }
 
-songsContainer.addEventListener('click', event => {
+const fetchLyrics = async (artist, songTitle) => {
+  const data = await fetchData(`${apiUrl}/v1/${artist}/${songTitle}`)
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '</br>')
+  insertLyricsIntoPage({ lyrics, artist, songTitle })
+}
+
+const handleSongsContainerClick = event => {
   const clickedElement = event.target
 
   if (clickedElement.tagName === 'BUTTON') {
@@ -86,4 +91,6 @@ songsContainer.addEventListener('click', event => {
     prevAndNextContainer.innerHTML = ''
     fetchLyrics(artist, songTitle)
   }
-})
+}
+
+songsContainer.addEventListener('click', handleSongsContainerClick)
